4cc80fcbf02b5906161dad4379de79f906ea5655
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-07-28 19:44:09 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  const body = document.querySelector('body')
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const menuItem = document.querySelectorAll('.menu__item')
+
+  if (burger) {
+    $(document).on('click', function (e) {
+      if (!$(e.target).closest('.active').length) {
+        burger.classList.remove('active')
+        menu.classList.remove('active')
+        body.classList.remove('no-scroll')
+      }
+      e.stopPropagation()
+    })
+
+    menuItem.forEach((item) => {
+      item.addEventListener('click', () => {
+        burger.classList.toggle('active')
+        menu.classList.remove('active')
+        body.classList.remove('no-scroll')
+      })
+    })
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active')
+      menu.classList.toggle('active')
+      body.classList.toggle('no-scroll')
+    })
+  }
+})
+// document.addEventListener('DOMContentLoaded', () => {})
